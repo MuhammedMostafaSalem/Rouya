74b4dcd1eab67e52c5ad9a13348cdf7d5bc9ebea
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-14 19:32:54 +0200

    create event change multilanguage

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
+// select multilingual in this site
+const translations = {
+    en : {
+        rouya: "Rouya",
+        home: "Home",
+        ourServices: "Our services",
+        ourClients: "Our clients",
+        aboutUs: "About us",
+        contactUs: "Contact us",
+        english: "English",
+        arabic: "Arabic",
+        textContent: "We exist to help people live better with artificial intelligence",
+    },
+    ar : {
+        rouya: "رؤية",
+        home: "الصفحة الرئيسية",
+        ourServices: "خدماتنا",
+        ourClients: "عملاؤنا",
+        aboutUs: "من نحن",
+        contactUs: "تواصل معنا",
+        english: "الانجليزية",
+        arabic: "العربية",
+        textContent: "نحن موجودون لمساعدة الناس على العيش بشكل أفضل باستخدام الذكاء الاصطناعي",
+    },
+};
+
+const select = document.querySelector('#language-selector');
+select.addEventListener("change", (e) => {
+    setLanguage(e.target.value);
+    localStorage.setItem('lang', e.target.value);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const language = localStorage.getItem('lang') || 'en';
+    setLanguage(language);
+});
+
+const setLanguage = (language) => {
+    const elements = document.querySelectorAll('[data-lang]');
+    elements.forEach((element) => {
+        const translationKey = element.getAttribute('data-lang');
+        element.textContent = translations[language][translationKey];
+    });
+    document.dir = language === 'ar' ? 'rtl' : 'ltr';
+};
+
+
+
+
 /* select navigation */
 window.addEventListener('scroll', ()=> {
     let navbar = document.querySelector('.navbar');
