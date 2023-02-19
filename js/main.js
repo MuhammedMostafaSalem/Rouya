df7b4f4e90f0691a5b4eca3ee5b37220cc30d332
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-02-20 00:17:16 +0200

    delete code reveal from main.js file

--- js/main.js
+++ js/main.js
@@ -478,27 +478,6 @@ navbarToggler.addEventListener('click' , ()=> {
 
 
 
-/* select reveal website elements on scroll */
-window.addEventListener('scroll', reveal);
-function reveal() {
-    let reveals = document.querySelectorAll('.reveal');
-    for(let i = 0; i <= reveals.length; i++) {
-        let windowHight = window.innerHeight;
-        let revealTop = reveals[i].getBoundingClientRect().top;
-        let revealPoint = 150;
-
-        if(revealTop < windowHight - revealPoint) {
-            reveals[i].classList.add('active');
-        }
-        else {
-            reveals[i].classList.remove('active');
-        }
-    }
-};
-
-
-
-
 /* select solutions section */
 let tabs = document.querySelectorAll('.tabs p');
 let tabsArr = Array.from(tabs);
